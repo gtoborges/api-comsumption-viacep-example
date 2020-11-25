3749e2ecda715418c59f058c315a374981f128d2
Augusto <gtoborges@gmail.com>
2020-11-25 20:29:18 -0300

    add verificação no cep

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">iacep
-    <input type=text v-model="cep"><button @click="buscarCep()">buscar</button>
+    <img alt="Vue logo" src="./assets/logo.png" style="margin-right: -80px; margin-bottom: -15px">iacep
+    <br>
+    <p style="margin-bottom: 0">Digite apenas os 8 dígitos do cep para buscar</p>
+    <input type=text v-model="cep" style="margin-top: 1rem; text-align: center"><button @click="buscarCep()">Buscar</button>
     <Endereco msg="Endereço encontrado: " :enderecoEncontrado="enderecoEncontrado" v-if="Object.keys(enderecoEncontrado).length"/>
   </div>
 </template>
@@ -26,6 +28,18 @@ export default {
   },
   methods: {
     async buscarCep() {
+      let digitos = this.cep.split('')
+      if(digitos.length != 8) {
+        console.log(isNaN(2))
+        alert('Não foram encontrados exatamente 8 dígitos')
+        this.enderecoEncontrado = {}
+        return
+      }
+      if(digitos.find(n => isNaN(Number(n)) )) {
+        alert('Algum dos 8 dígitos não é um número. Apenas números permitidos')
+        this.enderecoEncontrado = {}
+        return
+      }
       try {
         let response = await fetch(`https://viacep.com.br/ws/${this.cep}/json/`)
         let data = await response.json()
